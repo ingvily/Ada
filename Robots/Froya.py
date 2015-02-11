9c8a3a6bb4ff9c86c5d4c568469be0b3b3276b34
ingvily <ingvild.lyckander@gmail.com>
2015-02-11 05:33:19 +0100

    Update Froya.py
    Endret baserobot til norske kommentarer og kun nødvendige metoder

--- Robots/Froya.py
+++ Robots/Froya.py
@@ -6,25 +6,25 @@ from robot import Robot
 
 class Froya(Robot):
     
-    def init(self):     # Pimp Your Bot
-        
-        
-        #(Rød, Gul, Blå) fra 0 til 256
+    def sensors(self):  #Nødvendig for å kunne observere arenaen
+        pass
+
+    def init(self):     # Pimp Din Bot    
+        #(Rød, Gul, Blå) i verdier fra 0 til 255
         self.setColor(0, 0, 0)
-        self.setGunColor(256, 256, 256)
+        self.setGunColor(255, 255, 255)
         self.setRadarColor(0, 0, 0)
-        self.setBulletsColor(0, 0, 0)
+        self.setBulletsColor(200, 200, 200)
 
     
-    def run(self):      #main loop to command the bot
-        self.stop()
-       
-    def sensors(self): 
-        """Tick each frame to have datas about the game"""
-         
-    def onTargetSpotted(self, botId, botName, botPos):
-        "when the bot see another one"
+    def run(self):     
+        # Skriv din kode her for å gi roboten den oppførselen du ønsker
+        self.pause(10)      
+    
+  
+    def onHitWall(self):
+        # Erstatt "pass" med den oppførselen du ønsker 
+        # at roboten skal ha når den treffer veggen.  
+        pass
         
-    def onRobotDeath(self):#NECESARY FOR THE GAME
-        """When my bot die"""
-        self.rPrint ("damn I'm Dead")
+       

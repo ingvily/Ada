4c6acbc453c85b2dd021f42fee2acf75e1e54547
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-14 17:50:06 +0100

    Lagt til shell-skript for enkelt å git-oppdatere branchene seg imellom

--- skript_nar_master_endres.sh
+++ skript_nar_master_endres.sh
@@ -1,1 +1,72 @@
-python main.py lastArenaAll RobotsNorrone
+# Litt tungvint, med må nesten bare sørge for å isolere commits 
+# og cherrypicke den man vil
+
+
+# eller: det er bare shell-skritpene og robotene som blir endret på
+# kopiere over de når det skjer noe, kanskje? 
+
+
+# Oppdaterer branchen idunn: 
+git checkout Idunn 
+
+git checkout master Robots/easy.py
+git checkout master Robots/medium.py
+git checkout master Robots/hard.py
+
+git checkout master main.sh
+git checkout Spill_Lett.sh
+git checkout Spill_Middels.sh
+git checkout Spill_Vanskelig.sh
+
+
+# Oppdaterer branchen Odin: 
+git checkout Odin 
+
+git checkout master Robots/easy.py
+git checkout master Robots/medium.py
+git checkout master Robots/hard.py
+
+git checkout master main.sh
+git checkout Spill_Lett.sh
+git checkout Spill_Middels.sh
+git checkout Spill_Vanskelig.sh
+
+
+# Oppdaterer branchen Froya: 
+git checkout Froya 
+
+git checkout master Robots/easy.py
+git checkout master Robots/medium.py
+git checkout master Robots/hard.py
+
+git checkout master main.sh
+git checkout Spill_Lett.sh
+git checkout Spill_Middels.sh
+git checkout Spill_Vanskelig.sh
+
+
+# Oppdaterer branchen Tor: 
+git checkout Tor 
+
+git checkout master Robots/easy.py
+git checkout master Robots/medium.py
+git checkout master Robots/hard.py
+
+git checkout master main.sh
+git checkout Spill_Lett.sh
+git checkout Spill_Middels.sh
+git checkout Spill_Vanskelig.sh
+
+
+
+# Oppdaterer branchen Loke: 
+git checkout Loke 
+
+git checkout master Robots/easy.py
+git checkout master Robots/medium.py
+git checkout master Robots/hard.py
+
+git checkout master main.sh
+git checkout Spill_Lett.sh
+git checkout Spill_Middels.sh
+git checkout Spill_Vanskelig.sh
--- skript_nar_master_henter_inn_roboter_fra_branches.sh
+++ skript_nar_master_henter_inn_roboter_fra_branches.sh
@@ -1,1 +1,23 @@
-python main.py lastArenaAll RobotsNorrone
+#Må vi oppdatere alle? 
+
+# git checkout Idunn
+# git pull
+# git checkout Odin
+# git pull
+# git checkout Froya
+# git pull
+# git checkout Tor
+# git pull
+# git checkout Loke
+# git pull
+
+
+git checkout Idunn Robots/Idunn.py
+
+git checkout Odin Robots/Odin.py
+
+git checkout Tor Robots/Tor.py
+
+git checkout Loke Robots/Loke.py
+
+git checkout Froya Robots/Froya.py

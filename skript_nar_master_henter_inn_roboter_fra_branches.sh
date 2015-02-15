3b6eabc7d67b6160d89d58712fe9e6f3cfe7f102
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-15 19:47:17 +0100

    Endret på git-skript. Bli liggende kun i master

--- skript_nar_master_henter_inn_roboter_fra_branches.sh
+++ skript_nar_master_henter_inn_roboter_fra_branches.sh
@@ -21,3 +21,18 @@ git checkout Tor Robots/Tor.py
 git checkout Loke Robots/Loke.py
 
 git checkout Frøya Robots/Froya.py
+
+
+git reset HEAD 
+git stash
+
+git add Robots/Idunn.py
+git add Robots/Odin.py
+git add Robots/Tor.py
+git add Robots/Loke.py
+git add Robots/Froya.py
+
+git commit -m "Oppdatert basesrobotene ved å hente endringer i filene fra hver branch"
+
+git push 
+git stash pop

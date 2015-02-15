33b869fac287b3be067e473b52d54491c78fc7c1
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-15 20:02:34 +0100

    Endret på skript når master endres for å checke ut 3 commits i de andre branchene

--- skript_nar_master_endres.sh
+++ skript_nar_master_endres.sh
@@ -1,97 +1,111 @@
 # Litt tungvint, med må nesten bare sørge for å isolere commits 
 # og cherrypicke den man vil
 
-# git checkout Idunn
-# git cherry-pick (commit 55b9bbe5da59b84d)
-# git push
-# git checkout Odin
-# git cherry-pick (commit 55b9bbe5da59b84d)
-# git push
-# git checkout Tor
-# git cherry-pick (commit 55b9bbe5da59b84d)
-# git push
-# git checkout Frøya
-# git cherry-pick (commit 55b9bbe5da59b84d)
-# git push
-# git checkout Loke
-# git cherry-pick (commit 55b9bbe5da59b84d)
-# git push
+ git checkout Idunn
+ git cherry-pick bb51be1a55dbb621
+ git cherry-pick d6c70d3fa2a527a
+ git cherry-pick 2d8e76bfae6cb944
+ git push
+
+ git checkout Odin
+ git cherry-pick bb51be1a55dbb621
+ git cherry-pick d6c70d3fa2a527a
+ git cherry-pick 2d8e76bfae6cb944
+ git push
+ 
+ git checkout Tor
+ git cherry-pick bb51be1a55dbb621
+ git cherry-pick d6c70d3fa2a527a
+ git cherry-pick 2d8e76bfae6cb944
+ git push
+ 
+ git checkout Frøya
+ git cherry-pick bb51be1a55dbb621
+ git cherry-pick d6c70d3fa2a527a
+ git cherry-pick 2d8e76bfae6cb944
+ git push
+ 
+ git checkout Loke
+ git cherry-pick bb51be1a55dbb621
+ git cherry-pick d6c70d3fa2a527a
+ git cherry-pick 2d8e76bfae6cb944
+ git push
 
 
 # eller: det er bare shell-skritpene og robotene som blir endret på
 # kopiere over de når det skjer noe, kanskje? 
 
 
-# Oppdaterer branchen idunn: 
-git checkout Idunn 
+# # Oppdaterer branchen idunn: 
+# git checkout Idunn 
 
-git checkout master Robots/easy.py
-git checkout master Robots/medium.py
-git checkout master Robots/hard.py
+# git checkout master Robots/easy.py
+# git checkout master Robots/medium.py
+# git checkout master Robots/hard.py
 
-git checkout master main.sh
-git checkout Spill_Lett.sh
-git checkout Spill_Middels.sh
-git checkout Spill_Vanskelig.sh
+# git checkout master main.sh
+# git checkout Spill_Lett.sh
+# git checkout Spill_Middels.sh
+# git checkout Spill_Vanskelig.sh
 
-git push 
-
-
-# Oppdaterer branchen Odin: 
-git checkout Odin 
-
-git checkout master Robots/easy.py
-git checkout master Robots/medium.py
-git checkout master Robots/hard.py
-
-git checkout master main.sh
-git checkout Spill_Lett.sh
-git checkout Spill_Middels.sh
-git checkout Spill_Vanskelig.sh
-
-git push 
+# git push 
 
 
-# Oppdaterer branchen Frøya: 
-git checkout Frøya 
+# # Oppdaterer branchen Odin: 
+# git checkout Odin 
 
-git checkout master Robots/easy.py
-git checkout master Robots/medium.py
-git checkout master Robots/hard.py
+# git checkout master Robots/easy.py
+# git checkout master Robots/medium.py
+# git checkout master Robots/hard.py
 
-git checkout master main.sh
-git checkout Spill_Lett.sh
-git checkout Spill_Middels.sh
-git checkout Spill_Vanskelig.sh
+# git checkout master main.sh
+# git checkout Spill_Lett.sh
+# git checkout Spill_Middels.sh
+# git checkout Spill_Vanskelig.sh
 
-git push 
-
-
-# Oppdaterer branchen Tor: 
-git checkout Tor 
-
-git checkout master Robots/easy.py
-git checkout master Robots/medium.py
-git checkout master Robots/hard.py
-
-git checkout master main.sh
-git checkout Spill_Lett.sh
-git checkout Spill_Middels.sh
-git checkout Spill_Vanskelig.sh
-
-git push 
+# git push 
 
 
-# Oppdaterer branchen Loke: 
-git checkout Loke 
+# # Oppdaterer branchen Frøya: 
+# git checkout Frøya 
 
-git checkout master Robots/easy.py
-git checkout master Robots/medium.py
-git checkout master Robots/hard.py
+# git checkout master Robots/easy.py
+# git checkout master Robots/medium.py
+# git checkout master Robots/hard.py
 
-git checkout master main.sh
-git checkout Spill_Lett.sh
-git checkout Spill_Middels.sh
-git checkout Spill_Vanskelig.sh
+# git checkout master main.sh
+# git checkout Spill_Lett.sh
+# git checkout Spill_Middels.sh
+# git checkout Spill_Vanskelig.sh
 
-git push 
+# git push 
+
+
+# # Oppdaterer branchen Tor: 
+# git checkout Tor 
+
+# git checkout master Robots/easy.py
+# git checkout master Robots/medium.py
+# git checkout master Robots/hard.py
+
+# git checkout master main.sh
+# git checkout Spill_Lett.sh
+# git checkout Spill_Middels.sh
+# git checkout Spill_Vanskelig.sh
+
+# git push 
+
+
+# # Oppdaterer branchen Loke: 
+# git checkout Loke 
+
+# git checkout master Robots/easy.py
+# git checkout master Robots/medium.py
+# git checkout master Robots/hard.py
+
+# git checkout master main.sh
+# git checkout Spill_Lett.sh
+# git checkout Spill_Middels.sh
+# git checkout Spill_Vanskelig.sh
+
+# git push 

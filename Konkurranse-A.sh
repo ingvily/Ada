9d68e4c4f6eb5aa0e7509e13fa8d57d786311b90
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-16 22:03:25 +0100

    Lager skript for konkurransene

---
+++ Konkurranse-A.sh
@@ -0,0 +1,21 @@
+
+
+python main.py cup1A:F-L-O
+
+python main.py cup2A:F-L-T
+
+python main.py cup3A:F-L-I	
+
+python main.py cup4A:F-L-S
+
+python main.py cup5A:F-O-T
+
+python main.py cup6A:F-O-I	
+
+python main.py cup7A:F-O-S
+
+python main.py cup8A:F-T-I
+
+python main.py cup9A:F-T-S	
+
+python main.py cup10A:F-I-S	
---
+++ Konkurranse-B.sh
@@ -0,0 +1,21 @@
+
+
+python main.py cup1B:T-I-S
+
+python main.py cup2B:O-I-S
+
+python main.py cup3B:O-T-S	
+
+python main.py cup4B:O-T-I
+
+python main.py cup5B:S-L-I
+
+python main.py cup6B:S-L-T
+
+python main.py cup7B:I-L-T	
+
+python main.py cup8B:S-L-O
+
+python main.py cup9B:I-L-O	
+
+python main.py cup10B:T-L-O

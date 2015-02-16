d56833c6a18cd6da78e74dae5dd6eb253e28a230
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-16 21:57:49 +0100

    Ber skriptene lese inn arena-navn

--- Spill_Lett.sh
+++ Spill_Lett.sh
@@ -1,4 +1,12 @@
+<<<<<<< HEAD
 python main_lett.py
+=======
+#git add --all 
+#git commit -m "ny commit"
+#git push
+
+python main.py testArenaEasy
+>>>>>>> b22ef93... Ber skriptene lese inn arena-navn
 
 git add --all 
 git commit -m "ny commit"
--- Spill_Vanskelig.sh
+++ Spill_Vanskelig.sh
@@ -1,4 +1,13 @@
+<<<<<<< HEAD
 python main_vanskelig.py
+=======
+
+#git add --all 
+#git commit -m "ny commit"
+#git push
+
+python main.py testArenaHard
+>>>>>>> b22ef93... Ber skriptene lese inn arena-navn
 
 git add --all 
 git commit -m "ny commit"

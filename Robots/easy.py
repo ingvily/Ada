3343be7a1c49cc8133318225f9da872655d92d39
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-16 08:54:10 +0100

    Endret alle testroboter

--- Robots/easy.py
+++ Robots/easy.py
@@ -8,33 +8,48 @@ class EasyBot(Robot):
     
     def init(self):     # Pimp Your Bot
         
-        
         #(Rød, Gul, Blå) fra 0 til 256
         self.setColor(0, 200, 100)
         self.setGunColor(200, 200, 0)
-        self.setRadarColor(255, 60, 0)
+        self.setRadarColor(254, 60, 0)
         self.setBulletsColor(0, 200, 100)
 
     
     def run(self):      #main loop to command the bot
-
-        self.move(90) # for moving (negative values go back)
-        self.turn(360) #for turning (negative values turn counter-clockwise)
-        self.fire(5)
+        self.move(50)
+        self.fire(2) # To Fire (power between 1 and 10)
         self.stop()
-        """
-        the stop command is used to make moving sequences: here the robot will move 90steps and turn 360° at the same time
-        and next, fire
-        """
-       
+
+        self.turn(50)
+        self.gunTurn(50)
+        self.fire(2) # To Fireioioio (power between 1 and 10)
+        self.stop()
+
+        self.move(-30)
+
     def sensors(self): 
         """Tick each frame to have datas about the game"""
-         
+    
+    def onHitWall(self):
+        pass
+
     def onTargetSpotted(self, botId, botName, botPos):
         "when the bot see another one"
+        pass 
         
     def onRobotDeath(self):#NECESARY FOR THE GAME
         """When my bot die"""
         self.rPrint ("damn I'm Dead")
 
+    def onHitByBullet(self, bulletBotId, bulletBotName, bulletPower): #NECESARY FOR THE GAME
+        pass
+        
+    def onBulletHit(self, botId, bulletId):#NECESARY FOR THE GAME
+        pass
+        
+    def onBulletMiss(self, bulletId):
+        pass
+
+    def onRobotHit(self, robotId, robotName): # when My bot hit another
+        pass
 

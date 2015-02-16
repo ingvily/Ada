4ab4bc87d74b0533561fb7c095b7404a2ef7557b
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-02-16 08:51:06 +0100

    Endret alle testrobotene

--- Robots/hard.py
+++ Robots/hard.py
@@ -2,34 +2,11 @@
 #-*- coding: utf-8 -*-
 
 from robot import Robot 
+import math
 
 
 class HardBot(Robot):
     
-<<<<<<< HEAD
-    def init(self):     # Pimp Your Bot
-        
-        
-        #(Rød, Gul, Blå) fra 0 til 256
-        self.setColor(0, 200, 100)
-        self.setGunColor(200, 200, 0)
-        self.setRadarColor(255, 60, 0)
-        self.setBulletsColor(0, 200, 100)
-
-    
-    def run(self):      #main loop to command the bot
-        self.stop()
-       
-    def sensors(self): 
-        """Tick each frame to have datas about the game"""
-         
-    def onTargetSpotted(self, botId, botName, botPos):
-        "when the bot see another one"
-        
-    def onRobotDeath(self):#NECESARY FOR THE GAME
-        """When my bot die"""
-        self.rPrint ("damn I'm Dead")
-=======
     def init(self):    
 
         #(Rød, Gul, Blå) i verdier fra 0 til 255
@@ -103,4 +80,3 @@ class HardBot(Robot):
 
         self.stop()
         self.fire(5)
->>>>>>> 2ab292d... Endret easy-medium-hard-roboter
--- Robots/medium.py
+++ Robots/medium.py
@@ -8,12 +8,7 @@ class MediumBot(Robot):
     
     def init(self):    
         
-<<<<<<< HEAD
-        
-        #(Rød, Gul, Blå) fra 0 til 256
-=======
         #(Rød, Gul, Blå) i verdier fra 0 til 255
->>>>>>> 2ab292d... Endret easy-medium-hard-roboter
         self.setColor(0, 200, 100)
         self.setGunColor(200, 200, 0)
         self.setRadarColor(255, 60, 0)
@@ -26,20 +21,6 @@ class MediumBot(Robot):
         self.lockRadar("gun")
         
     
-<<<<<<< HEAD
-    def run(self):      #main loop to command the bot
-        self.stop()
-       
-    def sensors(self): 
-        """Tick each frame to have datas about the game"""
-         
-    def onTargetSpotted(self, botId, botName, botPos):
-        "when the bot see another one"
-        
-    def onRobotDeath(self):#NECESARY FOR THE GAME
-        """When my bot die"""
-        self.rPrint ("damn I'm Dead")
-=======
     def run(self): 
         self.gunTurn(180)
         self.stop()
@@ -73,4 +54,3 @@ class MediumBot(Robot):
         self.setRadarField("thin")
         self.stop()
         self.fire(3)
->>>>>>> 2ab292d... Endret easy-medium-hard-roboter

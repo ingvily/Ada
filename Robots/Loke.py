4ee6a3318ea5b2b5b4e89c3d31d5b02ddd0547de
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-03-01 11:19:57 +0100

    Tilbakestilt Loke

--- Robots/Loke.py
+++ Robots/Loke.py
@@ -9,7 +9,7 @@ class Loke(Robot):
     def sensors(self):  #Nødvendig for å kunne observere arenaen
         pass
 
-    def init(self):     # Pimp Din Bot    
+    def init(self):
         #(Rød, Gul, Blå) i verdier fra 0 til 255
         self.setColor(0, 0, 0)
         self.setGunColor(255, 255, 255)
@@ -20,36 +20,12 @@ class Loke(Robot):
     def run(self):     
         # Skriv din kode her for å gi roboten den oppførselen du ønsker
         self.pause(10)      
-        self.move(30)
-        self.turn(90)
-        self.fire(10)
-        self.move(30)
-        self.turn(90)
-        self.radarTurn(90)
-        self.fire(4)
-        self.gunTurn(90)
-        self.fire(4)
-        self.turn(90)
-        self.move(30)
-        self.gunTurn(90)
-        self.fire(4)
-        self.radarTurn(90)
-        self.fire(4)
-        self.move(90)
     
   
     def onHitWall(self):
         # Erstatt "pass" med den oppførselen du ønsker 
         # at roboten skal ha når den treffer veggen.  
-        self.turn(90)
-        self.move(30)
-        selv.turn(90)
-
-    def onTargetSpotted(self, botId, botName, botPos):
-        self.fire(10)
-        self.stopRadarTurn(10)
-        self.rPrint("Skytskyt")
+        pass
         
     def onRobotDeath(self):
-        
-      
+        self.rPrint ("Til Valhall!")

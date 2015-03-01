250afe68602d91b820ec919b3f003277f53fcc25
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-03-01 12:41:56 +0100

    rettet merge-kommentar

--- Spill_Middels.sh
+++ Spill_Middels.sh
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-python main_middels.py
-=======
-#git add --all 
-#git commit -m "ny commit"
-#git push
 
 python main.py testArenaMedium
->>>>>>> b22ef93... Ber skriptene lese inn arena-navn
 
 git add --all 
 git commit -m "ny commit"

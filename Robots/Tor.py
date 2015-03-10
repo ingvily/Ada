d64751303a1af1aab8a3f923e1b7cc3e859d0194
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-03-10 20:20:17 +0100

    oppdatert alle robotene

--- Robots/Tor.py
+++ Robots/Tor.py
@@ -15,6 +15,8 @@ class Tor(Robot):
         self.setGunColor(255, 255, 255)
         self.setRadarColor(0, 0, 0)
         self.setBulletsColor(200, 200, 200)
+        self.radarVisible(True) 
+        self.setRadarField("thin")
 
     
     def run(self):     

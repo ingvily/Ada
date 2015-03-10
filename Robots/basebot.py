6e52e1271297b030973a9a01eee1da38f847e9f0
ingvily <ingvild lyckander@mac-ingvildlyckander.local>
2015-03-10 20:11:07 +0100

    tom commit

--- Robots/basebot.py
+++ Robots/basebot.py
@@ -15,6 +15,8 @@ class BaseBot(Robot):
         self.setGunColor(255, 255, 255)
         self.setRadarColor(0, 0, 0)
         self.setBulletsColor(200, 200, 200)
+        self.radarVisible(True) 
+        self.setRadarField("thin")
 
     
     def run(self):     

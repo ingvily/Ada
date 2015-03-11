4f96dffb9759c899c518c44e218589241a7402a6
student <student@mac-rekruttering3.local>
2015-03-11 18:40:28 +0100

    test snotra

--- Robots/Snotra.py
+++ Robots/Snotra.py
@@ -22,6 +22,7 @@ class Snotra(Robot):
     def run(self):     
         # Skriv din kode her for å gi roboten den oppførselen du ønsker
         self.pause(10)      
+        self.move(10)
     
   
     def onHitWall(self):

573fa3bbf475ee52e3c02b55a968fa43c507c792
student <student@mac-rekruttering5.local>
2015-03-11 18:14:45 +0100

    test idunn

--- Robots/Idunn.py
+++ Robots/Idunn.py
@@ -20,7 +20,8 @@ class Idunn(Robot):
     
     def run(self):     
         # Skriv din kode her for å gi roboten den oppførselen du ønsker
-        self.pause(10)      
+        self.pause(10)    
+        self.move(10)  
     
   
     def onHitWall(self):

4146305ae76135e1a17b78a894f9e65e67e1ef8f
student <student@mac-rekruttering2.local>
2015-03-12 11:47:05 +0100

    Commit før lunsj

--- Robots/Odin.py
+++ Robots/Odin.py
@@ -11,25 +11,33 @@ class Odin(Robot):
 
     def init(self):     # Pimp Din Bot    
         #(Rød, Gul, Blå) i verdier fra 0 til 255
-        self.setColor(153, 0, 0)
-        self.setGunColor(255, 255, 255)
-        self.setRadarColor(0, 0, 0)
+        self.setColor(0, 0, 0)
+        self.setGunColor(153, 0, 0)
+        self.setRadarColor(153, 0, 0)
         self.setBulletsColor(200, 200, 200)
         self.radarVisible(True) 
         self.setRadarField("thin")
 
     
-    def run(self):     
-        # Skriv din kode her for å gi roboten den oppførselen du ønsker
-        self.pause(10)    
-    
+    def run(self): 
+        self.gunTurn(360)
+        self.radarTurn(360)
+        self.pause(2)
   
     def onHitWall(self):
-        # Erstatt "pass" med den oppførselen du ønsker 
-        # at roboten skal ha når den treffer veggen.  
-        pass
+        self.move(-100)
+        self.turn(180)
 
     def onRobotDeath(self):
         self.rPrint ("Til Valhall!")  
         
        
+    def onTargetSpotted(self, botId, botName, botPos):
+        self.fire(6)
+
+    def onHitByBullet(self, bulletBotId, bulletBotName, bulletPower):
+        self.move(90)
+        self.turn(90)
+        self.gunTurn(360)
+        self.radarTurn(360)
+

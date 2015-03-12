db473a79e3829c5cc1243c913ea27125b40a8d7a
student <student@mac-rekruttering2.local>
2015-03-12 08:46:31 +0100

    tilbakestilt odin

--- Robots/Odin.py
+++ Robots/Odin.py
@@ -22,7 +22,6 @@ class Odin(Robot):
     def run(self):     
         # Skriv din kode her for å gi roboten den oppførselen du ønsker
         self.pause(10)    
-        self.move(10)  
     
   
     def onHitWall(self):
